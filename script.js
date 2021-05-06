49c3082b0735ba0d67968bdcdede2498f069acc4
Cooler_2012 <cool.er.af@gmail.com>
2021-05-07 02:35:44 +0300

    add: slider

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const checkbox = document.getElementById('changeTheme');
 const wrapp = document.getElementById('wrapp');
 const wrap = document.getElementById('wrap');
-const howItWorks = document.getElementById('HowItWorks');
+const howItWorks = document.getElementById('howItWorks');
 const petsSlider = document.getElementById('PetsSlider');
 const stepsToPay = document.getElementById('stepsToPay');
 const coments = document.getElementById('coments');
@@ -17,10 +17,9 @@ checkbox.onclick = function() {
     mapImg.classList.toggle('grey-theme');
 };
 
-
 //               Slider block
 const arrSliderItems = ['eagle', 'panda', 'garilla', 'croc', 'fox', 'sloth', 'elef', 'kotia'];
-const sliderItems = document.querySelectorAll('.slider-elem')
+const sliderItems = document.querySelectorAll('.slider-elem');
 const slider = document.getElementById('slider');
 const eagle = document.getElementById('eagle');
 const panda = document.getElementById('panda');
@@ -40,11 +39,11 @@ rangeValue.innerHTML = rangeOutput;
 rangeValue.oninput = function() {
     let carrent = this.value;
     remClassActive();
-    rangeOutput.innerHTML = this.value + '/8';
+    rangeOutput.innerHTML = '0'+ this.value + '/08';
     let item = document.getElementById(`${arrSliderItems[carrent-1]}`);
     item.classList.add('active');
     changePosSliderItems();
- };
+};
 
 const searchIndex = function searchIndexItem () {
     let index = 0;
@@ -54,11 +53,11 @@ const searchIndex = function searchIndexItem () {
         };
     });
     return index;
-}
+};
 
 const changePosSliderItems = function moovieSlider () {
     let index = searchIndex();
-    const LeftParam = [0, -250, -445, -643, -835, -1030, -1226, -1420];
+    const LeftParam = [0, -252, -445, -643, -835, -1030, -1226, -1420];
     slider.style.left = LeftParam[index] + 'px';
 };
 
@@ -67,11 +66,14 @@ const remClassActive = function removeClassActive () {
         if (elem.classList.contains('active')) {
             elem.classList.remove('active')
         }
-    })
+    });
 };
+
 const addClassActive = function addClassActive (event) {
     remClassActive();
     event.currentTarget.classList.add('active');
+    let index = searchIndex()+1;
+    rangeOutput.innerHTML = '0' + index + '/08';
 };
 
 slider.addEventListener('click', changePosSliderItems);
@@ -83,3 +85,50 @@ fox.addEventListener('click', addClassActive);
 sloth.addEventListener('click', addClassActive);
 elef.addEventListener('click', addClassActive);
 kotia.addEventListener('click', addClassActive);
+
+//        Block Slider
+
+const btnPrew = document.getElementById('prewSlides');
+const btnNext = document.getElementById('nextSlides');
+const sliderWrap = document.getElementById('slider-wrap');
+
+const listingSliderNext = function listingNext () {
+    if (sliderWrap.style.left == ('-1272px')) {
+        sliderWrap.style.left = 0
+    } else {
+        sliderWrap.style.left = (-1272) + 'px';
+    }
+};
+const listingSliderPrew = function listingPrew () {
+    console.log( sliderWrap.style.left)
+    if (sliderWrap.style.left == '0px'||0 ) {
+        sliderWrap.style.left = (-1272) + 'px';
+    } else {
+         sliderWrap.style.left = 0;
+    }
+};
+
+btnNext.addEventListener('click', listingSliderNext);
+btnPrew.addEventListener('click', listingSliderPrew);
+const arrSliderBlock = ['pandaBlock', 'eagleBlock', 'garillaBlock', 'crocBlock', 'foxBlock', 'slothBlock', 'elefBlock', 'kotiaBlock'];
+const sliderBlock = document.querySelectorAll('.sliderBlock');
+const sliderRangeValue = document.getElementById('sliderRangeValue');
+const sliderRangeOutPut = document.getElementById('sliderRangeOutPut');
+sliderRangeValue.innerHTML = sliderRangeOutPut;
+
+sliderRangeValue.oninput = function() {
+    sliderBlock.forEach(item => {
+        if(item.classList.contains('target')) {
+            item.classList.remove('target');
+        }
+    })
+    let carrent = this.value;
+    sliderRangeOutPut.innerHTML = '0' + this.value + '/08' ;
+    let item = document.getElementById(`${arrSliderBlock[carrent-1]}`);
+    item.classList.add('target');
+    if(carrent > 4) {
+        sliderWrap.style.left = (-1272) + 'px';
+    } else {
+        sliderWrap.style.left = 0;
+    }
+};
